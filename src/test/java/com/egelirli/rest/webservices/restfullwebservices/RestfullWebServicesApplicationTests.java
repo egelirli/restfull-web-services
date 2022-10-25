daa9c9a3c64d752d3d4aa4e0c3d69f2a62712438
egelirli <egelirli@yahoo.com>
2022-10-25 21:57:41 +0300

    inital commit

---
+++ src/test/java/com/egelirli/rest/webservices/restfullwebservices/RestfullWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egelirli.rest.webservices.restfullwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
