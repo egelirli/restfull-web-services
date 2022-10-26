ffa66068fd1dec7c87a6ff77adb264bb88f5fa79
egelirli <egelirli@yahoo.com>
2022-10-26 20:49:13 +0300

    versioning

---
+++ src/main/java/com/egelirli/rest/webservices/restfullwebservices/versioning/PersonV1.java
@@ -0,0 +1,22 @@
+package com.egelirli.rest.webservices.restfullwebservices.versioning;
+
+public class PersonV1 {
+	private String name;
+
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonV1 [name=" + name + "]";
+	}
+
+	
+	
+}
